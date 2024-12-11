8e17fd6ffa2d143f94f2e4f20baa01bec2cb43c2
Yusuf Aktan <yusufaktn@outlook.com>
2024-12-11 14:21:50 +0300

    spring init

---
+++ src/main/java/com/aktanyusuf/starter/GalleryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktanyusuf/gallery_management_app/GalleryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.gallery_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
